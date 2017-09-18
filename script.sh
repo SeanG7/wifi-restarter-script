829c7bb49609d019601698b2032fb5d3bec5478c
Sean <gilmore.seven@gmail.com>
2017-09-18 21:24:03 +1000

    Update with conditional

--- script.sh
+++ script.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 COUNT=0
+FIRSTFAIL=true
 while true
 do
     if !(ping -q -c 1 -W 1 google.com.au >/dev/null); then
-    echo "IPv4 is down"
-    networksetup -setairportpower en0 off; networksetup -setairportpower en0 on
-    COUNT=$((COUNT + 1))
-    echo "Process ID "$$
-    echo "Wifi dropped $COUNT times"
-    echo
+        if $FIRSTFAIL; then
+            echo "IPv4 is down"
+            networksetup -setairportpower en0 off; networksetup -setairportpower en0 on
+            COUNT=$((COUNT + 1))
+            echo "Process ID "$$
+            echo "Wifi dropped $COUNT times"
+            echo
+            FIRSTFAIL=false
+        fi
+    else
+        FIRSTFAIL=true
     fi
     trap "echo Exited!; exit;" SIGINT SIGTERM
     sleep 1
